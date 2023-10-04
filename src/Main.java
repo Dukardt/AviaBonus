4e3c88ef3d2d82946647311535231ebc468d6cd3
Valery Dukardt <valeradukardt@mail.ru>
2023-10-04 17:01:39 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int totalAmount = 10_000;
+       int rublesPerMile = 20;
+       int bonusMiles = totalAmount / rublesPerMile ;
+
+       System.out.println("Итого начисленно бонусных миль: " + bonusMiles);
+    }
+}
