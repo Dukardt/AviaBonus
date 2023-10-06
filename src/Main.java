44d75347e0b7eafc240026c7d83185029c098d90
Valery Dukardt <valeradukardt@mail.ru>
2023-10-06 17:25:31 +0700

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-       int totalAmount = 10_000;
-       int rublesPerMile = 20;
-       int bonusMiles = totalAmount / rublesPerMile ;
+        int totalAmount = 10_000;
+        int rublesPerMile = 20;
+        int bonusMiles = totalAmount / rublesPerMile;
 
-       System.out.println("Итого начисленно бонусных миль: " + bonusMiles);
+        System.out.println("Итого начисленно бонусных миль: " + bonusMiles);
 
         int totalMiles = 500;
     }
